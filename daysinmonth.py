ad2ab9ab25bc87ef81457d64c7018a2cb8b90b20
J3drek79 <117525013+J3drek79@users.noreply.github.com>
2022-11-09 10:30:35 +0100

    Create daysinmonth.py

---
+++ daysinmonth.py
@@ -0,0 +1,11 @@
+miesiac = int(input("Podaj numer miesiąca: \n"))
+rok = int(input("Podaj rok: \n"))
+    
+if((miesiac==2) and ((rok%4==0)  or ((rok%100==0) and (rok%400==0)))):
+    print("Liczba dni w miesiacu to: 29");
+elif(miesiac==2):
+    print("Liczba dni w miesiacu to: 28");
+elif(miesiac==1 or miesiac==3 or miesiac==5 or miesiac==7 or miesiac==8 or miesiac==10 or miesiac==12):
+    print("Liczba dni w miesiacu to: 31");
+else :
+    print("Liczba dni w miesiacu to: 30");
